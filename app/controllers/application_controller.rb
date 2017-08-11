3dd06fec80befff5d2deebdfd95da9c5841ebd90
Max W <xypotion@gmail.com>
2017-08-10 18:37:20 -0700

    moved root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  def hello
-    render html: "hell world"
-  end
+  protect_from_forgery with: :exception;
 end
